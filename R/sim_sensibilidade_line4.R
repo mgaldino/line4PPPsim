a2e02d16e01320086f4978e60a497c54fe0b67d4
mgaldino <mcz.fea@gmail.com>
2018-11-21 12:12:52 -0800

    corrigi bug no código (faltava um argumento em white_noise e random_walk)

--- R/sim_sensibilidade_line4.R
+++ R/sim_sensibilidade_line4.R
@@ -37,8 +37,8 @@ sim_sensibilidade_line4 <- function (n_sim, num_years,t_0 = 2.14, ipc_0 = 1.1,
                                      type = "white_noise", start_seed){
 
   switch(type,
-         white_noise = white_noise(start_seed, mu, sd),
-         random_walk = random_walk(start_seed, mu, sd),
+         white_noise = white_noise(start_seed, num_years, mu, sd),
+         random_walk = random_walk(start_seed, num_years, mu, sd),
          constant = constant(start_seed, num_years))
 
   previsto  <- gen_forecast_revenue(num_years = 33, ajuste_inflacao = F)
