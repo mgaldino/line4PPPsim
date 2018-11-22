d7fd90f6851fc15b2ae5a7bd5788f4e4695aa8da
mgaldino <mcz.fea@gmail.com>
2018-11-22 18:57:01 -0200

    corrigi câmbio base

--- R/risco_cambial.R
+++ R/risco_cambial.R
@@ -6,6 +6,8 @@
 #' @param tx_c_base base exchange rate
 #' @param exp_camb percent of expostion to foreign currency
 #' @param exp_usd exposition to dollar
+#' @param cambio_real Logical. If TRUE, will use real exchange rate up to 2017, then 3.77 as prediction for the following years.
+#'  If not, will use tx_c_base and replicate.
 #'
 #' @return amount that could be paid due to variation in exchange rate
 #'
@@ -14,7 +16,7 @@
 #' @export risco_cambio
 
 
-risco_cambio <- function(sens_tx_c, tx_c_base = 3.77, exp_camb = .5, exp_usd = 1) {
+risco_cambio <- function(sens_tx_c, tx_c_base = 3.77, cambio_real=F, exp_camb = .5, exp_usd = 1) {
 
   valor_ep <- 17021.599
   ep_n <- c(0, valor_ep, rep(0, 31))
@@ -23,6 +25,14 @@ risco_cambio <- function(sens_tx_c, tx_c_base = 3.77, exp_camb = .5, exp_usd = 1
   pi_ep <- gen_interest_ep()
   pi_fin_lp1 <- gen_interest_lp(type_lp = 1)
   pi_fin_lp2 <- gen_interest_lp(type_lp = 2)
+
+  if(cambio_real) {
+    tx_c_base <- c(1.94833, 1.83416, 1.99833, 1.76083, 1.675, 1.95583, 2.15566,
+                   2.35333, 3.33083, 3.49, 3.1925, 3.62, 3.68, 3.66, 3.7, rep(3.77, 18))
+
+  } else {
+    tx_c_base <- rep(tx_c_base, 33)
+  }
 
   result <- -(ep_n + amortizacao_fin_lp1 + amortizacao_fin_lp2 + pi_ep + pi_fin_lp1 + pi_fin_lp2)*exp_usd*(tx_c_base - tx_c_base*sens_tx_c)*exp_camb
 
