7957fdab8e2d132a013a7c686f9aa02791163a40
mgaldino <mcz.fea@gmail.com>
2018-11-22 18:46:39 -0200

    corrigi bug na formula

--- R/risco_cambial.R
+++ R/risco_cambial.R
@@ -18,13 +18,13 @@ risco_cambio <- function(sens_tx_c, tx_c_base = 3.77, exp_camb = .5, exp_usd = 1
 
   valor_ep <- 17021.599
   ep_n <- c(0, valor_ep, rep(0, 31))
-  fin_lp1 <- gen_fin_lp1()
-  fin_lp2 <- gen_fin_lp2()
+  amortizacao_fin_lp1 <- c(0, 0, 0, 0, rep(45701.625, 8), rep(0, 21) )
+  amortizacao_fin_lp2 <- c(rep(0, 9), rep(70579.2, 5), rep(0, 19) )
   pi_ep <- gen_interest_ep()
   pi_fin_lp1 <- gen_interest_lp(type_lp = 1)
   pi_fin_lp2 <- gen_interest_lp(type_lp = 2)
 
-  result <- sum(ep_n + fin_lp1 + fin_lp1 + pi_ep + pi_fin_lp1 + pi_fin_lp2)*exp_usd*(tx_c_base - tx_c_base*sens_tx_c)*exp_camb
+  result <- -(ep_n + amortizacao_fin_lp1 + amortizacao_fin_lp2 + pi_ep + pi_fin_lp1 + pi_fin_lp2)*exp_usd*(tx_c_base - tx_c_base*sens_tx_c)*exp_camb
 
   return(result)
 
