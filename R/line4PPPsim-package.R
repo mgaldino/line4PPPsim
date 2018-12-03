ab1e96290666e7858c0364f9d062d941af66342f
mgaldino <mcz.fea@gmail.com>
2018-12-03 13:03:06 -0200

    documentacao do pacote

---
+++ R/line4PPPsim-package.R
@@ -0,0 +1,10 @@
+#' @keywords internal
+#' line4PPPsim: Simulando impacxto fiscal do metrô
+#'
+#' Funções para simular o impacto fiscal da linha 4 do metrô
+#' Simular impacto cambial
+#' Simular impacto do mecanism ode ajuste de demanda
+#' Simular multa por atraso
+#'
+#' @name line4PPPsim
+#' @docType package
