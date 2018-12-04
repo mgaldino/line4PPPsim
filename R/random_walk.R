2a5b943423d1405a5c2e577817f1afd6518cf2df
mgaldino <mcz.fea@gmail.com>
2018-12-04 06:39:20 -0200

    introduzi o beta

--- R/random_walk.R
+++ R/random_walk.R
@@ -1,11 +1,12 @@
 #' @title Simulates a random walk
 #'
-#' @description This package allows the user to run Monte Carlo simulation to assess the fiscal impact of lline 4 PPP in São Paulo.
+#' @description Auxiliar function. It computes a wanrdom walk, that is used to generate sensibility. In other words, its results are used as themultiplier of real demand. A value os 1.1 means for instance that the demand is 1.1 bigger than projected.
 #'
-#' @param start_seed A number
+#' @param start_seed A number. First point of the series.
 #' @param num_years A number
 #' @param mu A number
 #' @param sd A number
+#' @ param beta A number. Effect of past step on the next step.
 #'
 #' @return a vector of simulated random walk times series of size equal to num_years
 #'
@@ -14,13 +15,13 @@
 #' @export random_walk
 
 random_walk <- function(start_seed, num_years=33,
-                        mu = 0, sd = .05) {
+                        mu = 0, sd = .05, beta=1) {
 
   y <- start_seed
   for ( i in 2:num_years){
     erro <- rnorm(1, mu, sd)
 
-    y[i] <- y[i-1] + erro
+    y[i] <- beta*y[i-1] + erro
   }
   # truncando
   y <- ifelse(y < .6, .6,
